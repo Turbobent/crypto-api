5e7f4af1f1993e5d8ce9f3c1d8b1de02ca5a139c
Turbobent <kasp728h@edu.mercantec.dk>
2024-10-09 12:05:10 +0200

    can post currency

--- app/Http/Controllers/Api/V1/CryptocurrencyController.php
+++ app/Http/Controllers/Api/V1/CryptocurrencyController.php
@@ -30,7 +30,7 @@ class CryptocurrencyController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorecryptocurrenciesRequest $request)
+    public function store(StoreCryptocurrencyRequest $request)
     {
         $validated = $request->validated();
 
